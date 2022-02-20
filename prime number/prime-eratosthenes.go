1bbc9937d3c9231ccd666494c26313cdf59edaa7
alfiancikoa <sikaraeng@gmail.com>
2022-02-20 22:18:25 +0800

    prime eratosthenes

--- prime number/prime-eratosthenes.go
+++ prime number/prime-eratosthenes.go
@@ -6,12 +6,15 @@ import (
 )
 
 func showPrimes(n int) {
+	//  membuat tempat tampungan untuk bilanagn prima
 	prime := make([]bool, n+1)
+	// set seluruh array menjadi true
 	for i := 0; i <= n; i++ {
 		prime[i] = true
 	}
+	// set array ke 0 dan 1 menjadi false, karena bukan bilangan prima
 	prime[0], prime[1] = false, false
-	fmt.Println(prime)
+	// coret bilangan yang bukan prima dengan mengeset menjadi false
 	for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
 		if prime[i] {
 			for j := i * i; j <= n; j += i {
@@ -19,6 +22,7 @@ func showPrimes(n int) {
 			}
 		}
 	}
+	// tampilkan seluruh bilangan prima, nilai di dalam array adalah true
 	for i := 0; i <= n; i++ {
 		if prime[i] {
 			fmt.Print(i, " ")
@@ -28,5 +32,5 @@ func showPrimes(n int) {
 }
 
 func main() {
-	showPrimes(10)
+	showPrimes(100)
 }
