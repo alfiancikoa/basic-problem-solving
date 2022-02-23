a5c60f33d10d0c32c74b4bab3124bfd638cac068
alfiancikoa <sikaraeng@gmail.com>
2022-02-23 14:52:50 +0800

    most appear

---
+++ find most appear number/mostAppear.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func mostAppear(num []int) int {
+	hashmap := map[int]int{}
+	mostSoFar, mostNumber := 0, 0
+	for _, digit := range num {
+		hashmap[digit]++
+		if hashmap[digit] >= mostSoFar && digit >= mostNumber {
+			mostNumber = digit
+			mostSoFar = hashmap[digit]
+		}
+	}
+	return mostNumber
+}
+
+func main() {
+	fmt.Println(mostAppear([]int{90, 8, 90, 68, 101, 4, 1, 2, 101, 101})) // 101
+	fmt.Println(mostAppear([]int{92, 8, 92, 68, 100, 4, 1, 2, 100, 100})) // 100
+	fmt.Println(mostAppear([]int{5, 5, 1, 1, 2, 3, 10, 9}))               // 5
+	fmt.Println(mostAppear([]int{1, 2, 3, 4, 5, 6}))                      // 6
+	fmt.Println(mostAppear([]int{4, 3, 6, 2, 1, 5}))                      // 6
+}
