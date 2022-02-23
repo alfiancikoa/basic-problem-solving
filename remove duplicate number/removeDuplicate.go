c2c919ceb4efbf3ecdfcc7d3b901ee8eed1ee6a2
alfiancikoa <sikaraeng@gmail.com>
2022-02-23 15:25:25 +0800

    remove duplicate

---
+++ remove duplicate number/removeDuplicate.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+/*
+	Problem:
+	Hilangkan angka yang duplikat dan menampilkannya hanya sekali
+	- input:
+		deretan bilangan bulat yang dari 0 s/d n (0<n<=100)
+	- output:
+		deretan angka dalam array yang tidak duplikat dan terurut
+*/
+
+func removeDup(arr []int) []int {
+	var result []int
+	hashmap := map[int]bool{}
+	for _, digit := range arr {
+		if !hashmap[digit] {
+			result = append(result, digit)
+		}
+		hashmap[digit] = true
+	}
+	sort.Ints(result)
+	return result
+}
+
+func main() {
+	fmt.Println(removeDup([]int{1, 2, 2, 3, 4, 5, 5, 6, 7}))          // [1 2 3 4 5 6 7]
+	fmt.Println(removeDup([]int{2, 2, 2, 3, 3, 3, 4, 5, 5, 5}))       // [2,3,4,5]
+	fmt.Println(removeDup([]int{5, 4, 2, 1, 3}))                      // [1 2 3 4 5]
+	fmt.Println(removeDup([]int{2, 3, 2, 4, 1, 2, 5, 4, 2, 1, 6, 6})) // [1 2 3 4 5 6]
+}
