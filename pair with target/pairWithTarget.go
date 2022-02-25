31a767b7e4e40ca710a029c386e54e944ef2c7b2
alfiancikoa <sikaraeng@gmail.com>
2022-02-25 10:49:09 +0800

    pair with target

--- pair with target/pairWithTarget.go
+++ pair with target/pairWithTarget.go
@@ -4,15 +4,22 @@ import "fmt"
 
 /*
 	Problem:
-
+	Carilah dua angka apa saja yang terdapat di dalam arr[] yang jika dijumlahkan akan menghasilkan
+	angka sama dengan target
 	- input:
-
+		Terdapat dua parameter input yaitu:
+		# arr sebagai array kumpulan angka yang akan diproses untuk mencari angka pair dari target
+		# target merupakan angka yang akan menjadi patokan untuk mencari angka berapa saja yang ada di arr
+		yang jika dijumlahkan akan sama dengan target
+		# kedua parameter input terdiri dari bilangan bulat positif
 	- output:
-
+		# array yang terdiri dari dua angka yang jika kedua angka tersebut dijumlahkan
+		akan menghasilkan nilai yang sama dengan target.
+		# output hanya terdiri dari 2 angka
 	- example :
-		1.	#input 	= 		#output =
-		2.	#input 	= 		#output =
-		3.	#input 	= 		#output =
+		1.	#input 	= [1, 2, 3, 4, 5] , 6		#output = [2 4]
+		2.	#input 	= [2, 3, 4, 5, 6] , 8		#output = [3 5]
+		3.	#input 	= [2 3 4 5 6] , 1			#output = [-1]
 */
 
 func Pair(arr []int, target int) []int {
@@ -26,6 +33,9 @@ func Pair(arr []int, target int) []int {
 		}
 		hashmap[arr[i]] = i
 	}
+	if len(result) == 0 {
+		result = append(result, -1)
+	}
 	return result
 }
 
@@ -33,4 +43,5 @@ func main() {
 	fmt.Println(Pair([]int{1, 2, 3, 4, 5}, 6)) // [2 4]
 	fmt.Println(Pair([]int{2, 3, 4, 5, 6}, 8)) // [3 5]
 	fmt.Println(Pair([]int{2, 2, 3, 4, 4}, 7)) // [3 4]
+	fmt.Println(Pair([]int{2, 2, 3, 4, 4}, 1)) // [3 4]
 }
