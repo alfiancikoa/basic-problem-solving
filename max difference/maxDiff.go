e4805987327550a733777393ead7cd9b7cacb349
alfiancikoa <sikaraeng@gmail.com>
2022-02-26 12:33:31 +0800

    maxDiff

---
+++ max difference/maxDiff.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func maximumDiff(arr []int) int {
+	maxDiff, min := 0, arr[0]
+	for i := 0; i < len(arr); i++ {
+		if min > arr[i] {
+			min = arr[i]
+		} else if maxDiff < arr[i]-min {
+			maxDiff = arr[i] - min
+		}
+	}
+	return maxDiff
+}
+
+func main() {
+	fmt.Println(maximumDiff([]int{1, 2, 6, 4}))           // 5
+	fmt.Println(maximumDiff([]int{1, 3, 2, 4}))           // 3
+	fmt.Println(maximumDiff([]int{-1, -3, 2, 4}))         // 7
+	fmt.Println(maximumDiff([]int{3, 2, 4, 7}))           // 5
+	fmt.Println(maximumDiff([]int{2, 7, 9, 5, 1, 3, 5}))  // 7
+	fmt.Println(maximumDiff([]int{2, 7, 9, 5, 1, 5, 10})) // 9
+}
