34e2a9925fa5d0599960a92ab1c41465457b3f61
alfiancikoa <sikaraeng@gmail.com>
2022-02-26 12:37:45 +0800

    maxDiff

--- max difference/maxDiff.go
+++ max difference/maxDiff.go
@@ -11,9 +11,9 @@ import "fmt"
 	Output:
 		Hasil selisih maksimum yang ada pada deretan array input tersebut
 	Example:
-		1. #input: #output:
-		2. #input: #output:
-		3. #input: #output:
+		1. #input: [1, 2, 6, 4] 			#output: 5
+		2. #input: [-1, -3, 2, 4] 			#output: 7
+		3. #input: [2, 7, 9, 5, 1, 5, 10] 	#output: 9
 */
 
 func maximumDiff(arr []int) int {
