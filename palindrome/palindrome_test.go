d05ed1388ac5615bf438a373e4cfc8a6b80e6e75
alfiancikoa <sikaraeng@gmail.com>
2022-03-01 02:17:33 +0800

    palindrome

--- palindrome/palindrome_test.go
+++ palindrome/palindrome_test.go
@@ -1,8 +1,9 @@
-package main
+package palindrome
 
 import (
-	"fmt"
+	"github.com/stretchr/testify/assert"
 	"strings"
+	"testing"
 )
 
 /*
@@ -18,11 +19,11 @@ import (
 		3.	#input 	= "makan"	#output = false
 */
 
-func palindrome(str string) bool {
+func IsPalindrome(str string) bool {
 	// remove blank space and make it lower
 	input := strings.ToLower(strings.ReplaceAll(str, " ", ""))
 	leftIndex, rightIndex := 0, len(input)-1
-	for leftIndex <= rightIndex {
+	for leftIndex < rightIndex {
 		// jika karakter yang ada pada index kiri tidak sama dengan karakter index kanan maka langsung return false
 		if input[leftIndex] != input[rightIndex-leftIndex] {
 			return false
@@ -32,12 +33,21 @@ func palindrome(str string) bool {
 	return true
 }
 
-func main() {
-	fmt.Println(palindrome("kodok"))                // true
-	fmt.Println(palindrome("kasur ini rusak"))      // true
-	fmt.Println(palindrome("Kasur Koh Ahok rusak")) // true
-	fmt.Println(palindrome("Ibu Ratna antar ubi"))  // true
-	fmt.Println(palindrome("zoom"))                 // false
-	fmt.Println(palindrome("bubuk"))                // false
-	fmt.Println(palindrome("madam"))                // true
+func TestIsPalindrome(t *testing.T) {
+	t.Run("Test_IsPalindrome", func(t *testing.T) {
+		assert.Equal(t, true, IsPalindrome("kodok"))
+		assert.Equal(t, true, IsPalindrome("kasur ini rusak"))
+		assert.Equal(t, true, IsPalindrome("Kasur Koh Ahok rusak"))
+		assert.Equal(t, true, IsPalindrome("Ibu Ratna antar ubi"))
+		assert.Equal(t, false, IsPalindrome("zoom"))
+		assert.Equal(t, false, IsPalindrome("bubuk"))
+	})
+	// Result Pass
+}
+
+func BenchmarkFaktorial(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		IsPalindrome("Ibu Ratna antar ubi")
+	}
+	// Result 542.7 ns/op
 }
