763e6a221e19890f14237a6ac864a035726c480a
alfiancikoa <sikaraeng@gmail.com>
2022-03-01 02:02:42 +0800

    faktorial

--- faktorial/faktorial_test.go
+++ faktorial/faktorial_test.go
@@ -39,25 +39,29 @@ func FaktorialRecursive(total int, n int) int {
 */
 
 func TestFaktorial(t *testing.T) {
-	// Faktorial biasa
-	assert.Equal(t, 1, Faktorial(0))
-	assert.Equal(t, 1, Faktorial(1))
-	assert.Equal(t, 2, Faktorial(2))
-	assert.Equal(t, 6, Faktorial(3))
-	assert.Equal(t, 24, Faktorial(4))
-	assert.Equal(t, 120, Faktorial(5))
-	assert.Equal(t, 479001600, Faktorial(12))
-	// Result PASS
+	t.Run("Test_FaktorialBiasa", func(t *testing.T) {
+		// Faktorial biasa
+		assert.Equal(t, 1, Faktorial(0))
+		assert.Equal(t, 1, Faktorial(1))
+		assert.Equal(t, 2, Faktorial(2))
+		assert.Equal(t, 6, Faktorial(3))
+		assert.Equal(t, 24, Faktorial(4))
+		assert.Equal(t, 120, Faktorial(5))
+		assert.Equal(t, 479001600, Faktorial(12))
+		// Result PASS
+	})
+	t.Run("Test_FaktorialRecursive", func(t *testing.T) {
+		// Faktorial Recursive
+		assert.Equal(t, 1, FaktorialRecursive(1, 0))
+		assert.Equal(t, 1, FaktorialRecursive(1, 1))
+		assert.Equal(t, 2, FaktorialRecursive(1, 2))
+		assert.Equal(t, 6, FaktorialRecursive(1, 3))
+		assert.Equal(t, 24, FaktorialRecursive(1, 4))
+		assert.Equal(t, 120, FaktorialRecursive(1, 5))
+		assert.Equal(t, 479001600, FaktorialRecursive(1, 12))
+		// Result PASS
+	})
 
-	// Faktorial Recursive
-	assert.Equal(t, 1, FaktorialRecursive(1, 0))
-	assert.Equal(t, 1, FaktorialRecursive(1, 1))
-	assert.Equal(t, 2, FaktorialRecursive(1, 2))
-	assert.Equal(t, 6, FaktorialRecursive(1, 3))
-	assert.Equal(t, 24, FaktorialRecursive(1, 4))
-	assert.Equal(t, 120, FaktorialRecursive(1, 5))
-	assert.Equal(t, 479001600, FaktorialRecursive(1, 12))
-	// Result PASS
 }
 
 func BenchmarkFaktorial(b *testing.B) {
