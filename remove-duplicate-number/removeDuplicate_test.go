0c4109d66c3c33ab4d264f27b3fb706df9c2c585
alfiancikoa <sikaraeng@gmail.com>
2022-03-05 14:01:37 +0800

    remove duplicate

--- remove-duplicate-number/removeDuplicate_test.go
+++ remove-duplicate-number/removeDuplicate_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// Fungsi Remove Duplicate with Complexity O(n^2)
 func RmDupSolution1(nums []int) []int {
 	var result []int
 	sort.Ints(nums)
@@ -27,6 +28,7 @@ func RmDupSolution1(nums []int) []int {
 	return result
 }
 
+// Fungsi Remove Duplicate with Complexity O(n)
 func RmDupSolution2(nums []int) []int {
 	var result []int
 	sort.Ints(nums)
@@ -38,6 +40,26 @@ func RmDupSolution2(nums []int) []int {
 	result = append(result, nums[len(nums)-1])
 	return result
 }
+
+// Fungsi Remove Duplicate Using Data Struct (Hashmap) with Complexity O(n)
+func RmDupSolution3(nums []int) []int {
+	var result []int
+	var hash = map[int]bool{}
+	sort.Ints(nums)
+	for _, digit := range nums {
+		if !hash[digit] {
+			result = append(result, digit)
+		}
+		hash[digit] = true
+	}
+	return result
+}
+
+/*
+	*Note:
+	Untuk menjalankan Unit Test = go test -v
+	Untuk menjalankan Benchmark = go test -bench=.
+*/
 
 func Test_RemoveDuplicate(t *testing.T) {
 	t.Run("Test_RemoveDupSolution1", func(t *testing.T) {
@@ -56,4 +78,32 @@ func Test_RemoveDuplicate(t *testing.T) {
 		assert.Equal(t, []int{1, 2}, RmDupSolution2([]int{1, 1, 1, 2, 2, 1, 1, 2, 1}))
 		// PASS ok
 	})
+	t.Run("Test_RemoveDupSolution3", func(t *testing.T) {
+		assert.Equal(t, []int{1, 2, 3, 4, 5}, RmDupSolution3([]int{1, 1, 2, 3, 4, 4, 5}))
+		assert.Equal(t, []int{1, 2, 3, 4, 5, 6}, RmDupSolution3([]int{1, 1, 2, 2, 2, 3, 4, 4, 5, 6, 6, 6}))
+		assert.Equal(t, []int{1, 2, 3, 4, 5}, RmDupSolution3([]int{1, 3, 3, 2, 4, 4, 5}))
+		assert.Equal(t, []int{1, 2, 3, 4, 5, 6}, RmDupSolution3([]int{1, 3, 3, 2, 4, 4, 5, 5, 6, 6, 6}))
+		assert.Equal(t, []int{1, 2}, RmDupSolution3([]int{1, 1, 1, 2, 2, 1, 1, 2, 1}))
+		// PASS ok
+	})
+}
+
+// Benchmark untuk menghitung seberapa cepat program kita dieksekusi
+func Benchmark_RemoveDuplicateSolution1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RmDupSolution1(([]int{1, 3, 3, 2, 4, 4, 5, 5, 6, 6, 6}))
+	}
+	// Result 1622 ns/op
+}
+func Benchmark_RemoveDuplicateSolution2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RmDupSolution2(([]int{1, 3, 3, 2, 4, 4, 5, 5, 6, 6, 6}))
+	}
+	// Result 1199 ns/op
+}
+func Benchmark_RemoveDuplicateSolution3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RmDupSolution3(([]int{1, 3, 3, 2, 4, 4, 5, 5, 6, 6, 6}))
+	}
+	// Result 1802 ns/op
 }
