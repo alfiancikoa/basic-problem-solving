dacccb86e318eed15a64660530542bc4d104741e
alfiancikoa <sikaraeng@gmail.com>
2022-03-05 12:22:56 +0800

    most appear number

--- find-most-appear-number/mostAppear_test.go
+++ find-most-appear-number/mostAppear_test.go
@@ -27,17 +27,18 @@ func mostAppearNumber(nums []int) int {
 */
 
 // Unit Test dari Fungsi MostAppearNumber di Atas
-func TestMostAppearNumber(t *testing.T) {
+func Test_MostAppearNumber(t *testing.T) {
 	assert.Equal(t, 1, mostAppearNumber([]int{1, 1, 1, 1, 1}))
 	assert.Equal(t, 101, mostAppearNumber([]int{90, 8, 90, 68, 101, 4, 1, 2, 101, 101}))
 	assert.Equal(t, 100, mostAppearNumber([]int{92, 8, 92, 68, 100, 4, 1, 2, 100, 100}))
 	assert.Equal(t, 5, mostAppearNumber([]int{5, 5, 1, 1, 2, 3, 10, 9}))
 	assert.Equal(t, 6, mostAppearNumber([]int{1, 2, 3, 4, 5, 6}))
 	assert.Equal(t, 6, mostAppearNumber([]int{4, 3, 6, 2, 1, 5}))
+	// PASS ok
 }
 
 // Benchmark untuk mengetahui seberapa cepat program kita dieksekusi
-func BenchmarkMostAppearNumber(b *testing.B) {
+func Benchmark_MostAppearNumber(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		mostAppearNumber([]int{92, 8, 92, 68, 100, 4, 1, 2, 100, 100})
 	}
