9c46d5ddc99b3d91d0c1b42787585d0a83eb9c1d
alfiancikoa <sikaraeng@gmail.com>
2022-03-08 22:38:23 +0800

    linear search

--- linear-search/linearSearch_test.go
+++ linear-search/linearSearch_test.go
@@ -8,6 +8,11 @@ import (
 
 // Fungsi untuk mencari suatu bilangan dan mengembalikan index-nya
 func LinearSearch(arr []int, target int) int {
+	for i := 0; i < len(arr); i++ {
+		if target == arr[i] {
+			return i
+		}
+	}
 	return -1
 }
 
@@ -20,5 +25,15 @@ func LinearSearch(arr []int, target int) int {
 // Unit Test dari fungsi Linear Search di atas
 func Test_LinearSearch(t *testing.T) {
 	assert.Equal(t, -1, LinearSearch([]int{}, 5))
+	assert.Equal(t, 3, LinearSearch([]int{1, 2, 3, 4, 5}, 4))
+	assert.Equal(t, 6, LinearSearch([]int{2, 3, 4, 1, 6, 5, 7, 8, 9}, 7))
 
+}
+
+// Benchmark untuk menghitung seberapa cepat program kita dieksekusi
+func Benchmark_BinarySearch(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		LinearSearch([]int{2, 3, 4, 1, 6, 5, 7, 8, 9}, 7)
+	}
+	// Result 16.82 ns/op
 }
