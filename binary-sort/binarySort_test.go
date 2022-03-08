1c0b94682295247af5996e4925f3212e98a089ac
alfiancikoa <sikaraeng@gmail.com>
2022-03-08 22:14:13 +0800

    binary sort

--- binary-sort/binarySort_test.go
+++ binary-sort/binarySort_test.go
@@ -41,4 +41,13 @@ func Test_BinarySort(t *testing.T) {
 	assert.Equal(t, []int{1, 2, 3, 4, 5}, BinarySort([]int{5, 4, 2, 3, 1}))
 	assert.Equal(t, []int{1, 2, 3, 4, 5}, BinarySort([]int{5, 1, 3, 2, 4}))
 	assert.Equal(t, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, BinarySort([]int{2, 3, 1, 5, 10, 4, 7, 8, 6, 9}))
+	// PASS ok
+}
+
+// Benchmark untuk menghitung seberapa cepat program kita dieksekusi
+func Benchmark_BinarySearch(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		BinarySort([]int{2, 3, 1, 5, 10, 4, 7, 8, 6, 9})
+	}
+	// Result 4599 ns/op
 }
